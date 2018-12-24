3a4e796ab7e718893be0a003ccb464b61fd784e1
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2018-12-24 16:44:04 +0100

    First import.

---
+++ French/clean.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+\rm -f *.log *.dvi *.dvi0 *.ps *.aux
+
+\rm -f caballo_fr.pdf \
+       la_comete.pdf \
+       perigee_de_Mercure.pdf \
+       dualite.pdf \
+       le_cimetiere_des_etoiles.pdf \
+       pierrot.pdf \
+       encore_un_matin.pdf \
+       les_vers_blancs.pdf \
+       roma_mio_fr.pdf \
+       haikus.pdf \
+       mille_et_une_voix.pdf \
+       singing_in_the_rain.pdf \
+       hermine.pdf \
+       miroirs.pdf \
+       un_feu_nouveau.pdf \
+       journal.pdf \
+       nudite.pdf \
+       zebre.pdf
---
+++ Spanish/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+\rm -f *.log *.dvi *.dvi0 *.ps *.aux
+
+\rm -f caballo.pdf \
+       diario.pdf \
+       espejos.pdf \
+       roma_mio.pdf
