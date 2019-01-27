43e4f561a533626d3d83ec4562dcd9686b751fb9
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2019-01-27 17:02:29 +0100

    Added build script.

---
+++ French/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -x
+
+src="les_vers_blancs \
+     dualite \
+     hermine \
+     la_comete \
+     le_cimetiere_des_etoiles \
+     mille_et_une_voix \
+     perigee_de_Mercure \
+     pierrot \
+     un_feu_nouveau \
+     zebre \
+     tsuyu_fr"
+
+for TEX in $src; do
+  make pdf TEX=$TEX
+done
