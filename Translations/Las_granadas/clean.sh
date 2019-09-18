c30b667ad4f14bc0e67f976c662127cb19be08c3
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2019-09-18 16:46:47 +0200

    First import of P. Valery.

---
+++ Translations/Las_granadas/clean.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+\rm -f *.log *.dvi *.dvi0 *.ps *.aux
+
+\rm -f las_granadas.pdf \
+         biographie.pdf \
+         introduction.pdf \
+         l_amateur_de_poemes.pdf \
+         feerie.pdf \
+         meme_feerie.pdf \
+         au_bois_dormant.pdf \
+         le_bois_amical.pdf \
+         un_feu_distinct.pdf \
+         vue.pdf \
+         l_abeille.pdf \
+         les_pas.pdf \
+         la_ceinture.pdf \
+         le_sylphe.pdf \
+         l_insinuant.pdf \
+         les_grenades.pdf \
+         le_vin_perdu.pdf \
+         interieur.pdf \
+         colloque.pdf \
+         insinuant_II.pdf \
+         la_caresse.pdf
