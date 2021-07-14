84abd8ea96ef534b65928de456d1bacaaebdb3c3
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2021-07-14 16:24:34 +0200

    Rewrites. Added poems.pdf and its script.

---
+++ English/make.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+~/git/Scripts/combine_pdf.sh poems.pdf \
+                             cup.pdf \
+                             jars.pdf \
+                             tenth.pdf \
+                             one_two.pdf \
+                             regrets.pdf \
+                             adlib.pdf \
+                             comet.pdf \
+                             duality.pdf \
+                             tempo.pdf \
+                             fountain.pdf \
+                             hair.pdf \
+                             ermine.pdf \
+                             blue.pdf \
+                             meltwater.pdf \
+                             white.pdf \
+                             perigee.pdf
