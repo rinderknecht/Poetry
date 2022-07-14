46dc2d52e080115815ab4141df6afa723d2d663a
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2022-07-14 21:20:09 +0200

    Removed clean.sh and build.sh

--- Spanish/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#set -x
-
-src="espejos roma_mio diario caballo tsuyu_es"
-
-for TEX in $src; do
-  make pdf TEX=$TEX
-done
--- Spanish/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-\rm -f *.log *.dvi *.dvi0 *.ps *.aux
-
-symlinks.sh --unset
